ce14d2470a5743f9239683f5fab469a6f191b07b
Bernardo Hummes <hummescmpa@gmail.com>
2020-09-07 13:22:12 -0300

    added draw functions

--- src/main.cpp
+++ src/main.cpp
@@ -5,16 +5,11 @@
 //using namespace std;
 
 
-void draw_road(double x, double y, double l)
-{
-    double r = l / (2 * M_PI);
-    double r1 = r - 2.5;
-    double r2 = r + 2.5;
+void draw_road(double x, double y, double l);
 
-    vibes::clearFigure("Road");
-    vibes::drawCircle(x, y, r1);
-    vibes::drawCircle(x, y, r2);
-}
+void draw_robot(double pos, double r);
+
+double f(float x, float v, float u, float& xdot, float& vdot);
 
 int main(int argc, char *argv[])
 {
@@ -22,6 +17,7 @@ int main(int argc, char *argv[])
     float dt = 0.05;
     int vo = 3;
     int u = 1;
+    double r = l / (2 * M_PI);  // defines radius based on desired circunference
 
     vibes::beginDrawing();
     vibes::newFigure("Road");
@@ -31,11 +27,37 @@ int main(int argc, char *argv[])
                               );
     vibes::axisLimits(-20., 20., -20., 20.);
 
-    draw_road(0,0,5);
+    draw_road(0,0,r);
 
+    draw_robot(20,r);
 
 
 
     vibes::endDrawing();
     return 0;
+}
+
+double f(float x, float v, float u, float& xdot, float& vdot)
+{
+   return 1.0;
+}
+
+void draw_robot(double pos, double r)
+{
+    double th = pos / r;       // theta defined by position on the road
+    double px = r * cos(th);   // pos x in the plane
+    double py = r * sin(th);   // pos x in the plane
+    double pr = th + (M_PI/2);
+
+    vibes::drawTank(px, py, pr, 4, "black[white]", vibesParams("figure", "Road"));
+}
+
+void draw_road(double x, double y, double r)
+{
+    double r1 = r - 2.5;        // two circles will be 2.5 units distant from the middle of the road
+    double r2 = r + 2.5;        //  so that to have 5 width
+
+    vibes::clearFigure("Road");
+    vibes::drawCircle(x, y, r1);
+    vibes::drawCircle(x, y, r2);
 }
